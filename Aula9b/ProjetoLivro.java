3e3ef8a378ae9dcd6b9de424d50c1b336afa68a1
Ed <eduardoodferreiradev@gmail.com>
2024-08-01 17:02:05 -0300

    Quase terminado

---
+++ Aula9b/ProjetoLivro.java
@@ -0,0 +1,6 @@
+package Aula9b;
+
+public class ProjetoLivro {
+    Pessoa[] p = new Pessoa[3];
+    Livro[] l = new Livro[3];
+}
--- Aula9b/Publicacao.java
+++ Aula9b/Publicacao.java
@@ -1,13 +1,9 @@
 package Aula9b;
 
-public class Publicacao {
-    
+public interface Publicacao {
+    public void abrir();
+    public void fechar();
+    public void folhear(int p);
+    public void avancarPag();
+    public void voltarPag();
 }
-/*
-interface do livro 
-+ abrir()
-+ fechar 
-+ folhear
-+ avançarPag
-+ voltarPAg
- */
